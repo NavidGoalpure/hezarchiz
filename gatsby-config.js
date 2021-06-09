e6360be2f8e1f28dad8f826dd1b74adacdf166bc
navidgoalpure <navid.goalpure@gmail.com>
2021-06-09 11:51:29 +0430

    add simprq podcast

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `هزارچیز`,
-    description: `گردآوردنده پروژه های بلاکچین ایران`,
+    description: `مرجع معرفی پروژه های بلاکچین ایران`,
     siteUrl: `https://hezarchiz.com`,
     keywords: ["هزارچیز", "لیست پروژه های بلاکچین", "بلاکچین"],
     social: {
